e58fba41f2b9eb84bc3cbee3ed123e0abc11ca36
Mark Juers <mpjuers@indiana.edu>
2018-09-17 15:01:55 -0400

    linted runmsandParse.py

--- Code/runmsandParse.py
+++ Code/runmsandParse.py
@@ -1,6 +1,6 @@
 # runs MS and parses output a specified number of times using a parameter file
 # writes fst mean and variance to outfile csv with headers
-## nchrom is PER POPULATION
+# nchrom is PER POPULATION
 # usage retrieved from help flag
 
 from __future__ import print_function
@@ -9,39 +9,40 @@ import shlex
 import argparse
 import itertools as it
 import sys
-import pdb
 from subprocess import Popen, PIPE
 from multiprocessing import Pool
 from functools import partial
 
 
 def getH(population):
-    # returns a 2 x nseg list of first, mutant allele frequencies, 
+    # returns a 2 x nseg list of first, mutant allele frequencies,
     # second, heterozygosities
     p = [weightedMean(x) for x in zip(*population)]
     H = [2 * freq * (1 - freq) for freq in p]
     return [p, H]
 
 
-def weightedMean(values, weights = None):
+def weightedMean(values, weights=None):
     if weights:
-        return sum([values[i] * weights[i] 
-            for i in xrange(len(values))]) / sum(weights)
+        return sum([values[i] * weights[i]
+                    for i in range(len(values))]) / sum(weights)
     else:
         return sum(values) / len(values)
 
 
-def weightedVar(values, weights = None):
+def weightedVar(values, weights=None):
     V1 = float(sum(weights))
     xBar = weightedMean(values, weights)
     try:
         if weights:
-            return sum(weights[i] * (values[i] - xBar) ** 2 
-                    for i in xrange(len(values))) / (V1 - 1.0)
+            return sum(
+                    weights[i] * (values[i] - xBar) ** 2
+                    for i in range(len(values))) / (V1 - 1.0)
         else:
-            return sum((values[i] - xBar) ** 2 
-                for i in xrange(len(values))) / (float(len(values)) - 1.0)
-    except:
+            return sum(
+                (values[i] - xBar) ** 2
+                for i in range(len(values))) / (float(len(values)) - 1.0)
+    except Exception:
         return 0
 
 
@@ -55,14 +56,14 @@ def map_level(f, item, level):
 
 def getFst(H, segsites, nchrom, npop):
     # returns mean Gst over all loci using the "ratio of averages" method
-    # H is a npop x 2 x nseg list in the form 
+    # H is a npop x 2 x nseg list in the form
     # the first subindex is the site; the second is the population
-    # [[[p11, p21], 
+    # [[[p11, p21],
     #   [h11, h21]],
-    #   [[p12, p22], 
+    #   [[p12, p22],
     #   [h21, h22]]]
     HBySite = [zip(*x) for x in zip(*H)]
-    # [[[p11, p12], 
+    # [[[p11, p12],
     #   [p21, p22]],
     #  [[h11, h12],
     #   [h21, h22]]]
@@ -71,20 +72,21 @@ def getFst(H, segsites, nchrom, npop):
     #  [hbar1, hbar2]
     if HBarPerSite:
         pBar = HBarPerSite[0]
-        Ht = [(nchrom * npop) / (nchrom * npop - 1) * 2 * p * (1 - p) \
+        Ht = [
+                (nchrom * npop) / (nchrom * npop - 1) * 2 * p * (1 - p)
                 for p in pBar]
         Hs = [nchrom / (nchrom - 1) * x for x in HBarPerSite[1]]
         HtBar = weightedMean(Ht)
         HsBar = weightedMean(Hs)
         return 1 - HsBar / HtBar
-    else: 
+    else:
         return 0
 
 
 def getOutput(cmd):
     # runs ms using cmd and returns output
     args = shlex.split(cmd)
-    proc = Popen(args, stdout = PIPE, stderr = PIPE)
+    proc = Popen(args, stdout=PIPE, stderr=PIPE)
     out, err = proc.communicate()
     exitcode = proc.returncode
     return exitcode, out, err
@@ -97,12 +99,14 @@ def getPi(populations):
         segsites = range(len(A[0]))
         terms = []
         for i in range(len(A)):
-            p_i = float(len([1.0 for x in aHash if x == aHash[i]])) / float(len(A))
+            p_i = float(len([1.0 for x in aHash if x == aHash[i]])) \
+                / float(len(A))
             for j in range(i + 1, len(populations)):
                 p_j = float(len([1.0 for x in aHash if x == aHash[j]])) \
-                        / float(len(A))
+                    / float(len(A))
                 ids = tuple(it.chain(*[zip(segsites, A[x]) for x in (i, j)]))
-                pi_ij = float(len(set(ids)) - len(segsites)) / float(len(segsites))
+                pi_ij = float(len(set(ids)) - len(segsites)) \
+                    / float(len(segsites))
                 terms.append(p_i * p_j * pi_ij)
         return 2 * sum(terms)
     else:
@@ -122,39 +126,40 @@ def getSummary(row, args):
     populations = []
     npop = 0
     tau, rho = row
-    # const is a factor by which to multiply host theta and M to achieve 
+    # const is a factor by which to multiply host theta and M to achieve
     # symbiont effective population size
     const = rho / (tau ** 2 * (3 - 2 * tau) * (2 - rho) + rho)
     reps = {"total": args.nchrom * args.npop, "nsamp": args.nsamp,
-         "theta": args.theta * const, "npop": args.npop, 
-         "mig": args.mig * const, "nchrom": args.nchrom}
-    cmd = " ".join(("ms %(total)d %(nsamp)d -t %(theta)f -I %(npop)d" % reps, 
-        " ".join(str(args.nchrom) for x in range(args.npop)),
-        "%(mig)f" % reps))
+            "theta": args.theta * const, "npop": args.npop,
+            "mig": args.mig * const, "nchrom": args.nchrom}
+    cmd = " ".join(
+        ("ms %(total)d %(nsamp)d -t %(theta)f -I %(npop)d" % reps,
+            " ".join(str(args.nchrom) for x in range(args.npop)),
+            "%(mig)f" % reps))
     # get ms stdout
     msOut = getOutput(cmd)[1].split("\n")
-    
+
     for line in msOut:
+        if line.strip() == "//":
+            if H:
+                fst.append(getFst(H, segsites, args.nchrom, npop))
+                pi.append(getPi(populations))
+            i = 0
+            npop = 0
+            H = []
+            populations = []
         # add sequence lines to population
-        if ind.match(line):
+        elif ind.match(line):
             population.append([float(x) for x in line.strip()])
             i += 1
             # calculate H and add to metapopulation
             if i == args.nchrom:
                 i = 0
-                npop +=1
+                npop += 1
                 if population:
                     H.append(getH(population))
                     populations.append(population)
                 population = []
-        elif line.strip() == "//":
-            if H:
-                fst.append(getFst(H, segsites, args.nchrom, npop))
-                pi.append(getPi(populations))
-            i = 0 
-            npop = 0
-            H = []
-            populations = []
         elif line.startswith("segsites"):
             segsites.append(float(line.lstrip("segsites: ")))
     fst.append(getFst(H, segsites, args.nchrom, npop))
@@ -169,46 +174,55 @@ def getSummary(row, args):
 
 
 def summaryPool(args):
-    return partial(getSummary, args = args)
-           
-            
+    return partial(getSummary, args=args)
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("params", 
-            help = "The path of a .csv file containing float values of tau and rho,\
-                    one line per MS iteration. Headers will be stripped")
-    parser.add_argument("outfile",
-            nargs = "?",
-            default = None, 
-            help = "Output file")
-    parser.add_argument("-n",
+    parser.add_argument(
+            "params",
+            help="The path of a .csv file containing float values \
+                of tau and rho,\
+                one line per MS iteration. Headers will be stripped")
+    parser.add_argument(
+            "outfile",
+            nargs="?",
+            default=None,
+            help="Output file")
+    parser.add_argument(
+            "-n",
             "--nchrom",
-            type = int,
-            help = "Number of chromosomes sampled from each population")
-    parser.add_argument("-p",
+            type=int,
+            help="Number of chromosomes sampled from each population")
+    parser.add_argument(
+            "-p",
             "--npop",
-            type = int,
-            help = "Number of populations per sample")
-    parser.add_argument("-s",
+            type=int,
+            help="Number of populations per sample")
+    parser.add_argument(
+            "-s",
             "--nsamp",
-            type = int,
-            help = "Number of samples per iteration")
-    parser.add_argument("-t",
+            type=int,
+            help="Number of samples per iteration")
+    parser.add_argument(
+            "-t",
             "--theta",
-            type = float,
-            help = "Value of theta (2*Ne*u) for the host population")
-    parser.add_argument("-M",
+            type=float,
+            help="Value of theta (2*Ne*u) for the host population")
+    parser.add_argument(
+            "-M",
             "--mig",
-            type = float,
-            help = "Population migration parameter (2*Ne*m) \
+            type=float,
+            help="Population migration parameter (2*Ne*m) \
                 for the host population"
             )
-    parser.add_argument("-c",
-            "--ncore", 
-            nargs = "?",
-            default = 1,
-            type = int,
-            help = "Number of processing cores")
+    parser.add_argument(
+            "-c",
+            "--ncore",
+            nargs="?",
+            default=1,
+            type=int,
+            help="Number of processing cores")
     args = parser.parse_args()
 
     params = []
@@ -216,25 +230,22 @@ def main():
         for line in pars:
             try:
                 # added filter line to remove newlines
-                params.append([float(x) for x in 
-                    filter(None, line.strip().split(","))])
-            except:
+                params.append(
+                        [float(x) for x in
+                            filter(None, line.strip().split(","))])
+            except Exception:
                 pass
 
     pool = Pool(args.ncore)
     summaries = pool.map(summaryPool(args), params)
-    
+
     out = open(args.outfile, "w") if args.outfile else sys.stdout
     out.write("mean.fst, var.fst, pi, tau, rho\n")
     out.writelines(", ".join(str(i) for i in j) + "\n" for j in summaries)
     if out is not sys.stdout:
-        close(out)
+        out.close()
 
 
 if __name__ == "__main__":
     ind = re.compile('[01]+$')
     main()
-
-
-
-
