20fb27f199d42a0627983f5ff64bb78549bd917f
mpjuers <mpjuers@indiana.edu>
2018-10-02 20:37:41 -0400

    MetaSample.fst() written, untested

--- Python2/transmission.py
+++ Python2/transmission.py
@@ -318,6 +318,25 @@ class MetaSample(Sample):
             )
         self.populations = populations
 
+    def fst(self, method="gst", average_first=True, **kwargs):
+        if method == "gst":
+            h_by_rep = self.h(by_population=True, average=True, **kwargs)
+            if average_first:
+                hbar_by_rep = np.average(
+                        h_by_rep, axis=1, weights=self.segsites()
+                    )
+                hs = np.mean(hbar_by_rep)
+                ht = np.average(
+                        self.h(average=True, **kwargs), weights=self.segsites()
+                        )
+                return 1 - hs / ht
+            else:
+                hs = np.mean(h_by_rep, axis=0)
+                ht = self.h(average=True, **kwargs)
+                return np.average((1 - hs / ht), weights=self.segsites)
+        else:
+            raise Exception("unsupported method")
+
 
 def main():
     population_configurations = [ms.PopulationConfiguration(10)
