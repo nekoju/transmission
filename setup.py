e5b6abcd5beadb7cb0239babe6db383e73030099
mpjuers <mpjuers@indiana.edu>
2018-12-27 21:27:21 -0500

    fixed indentation in setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='transmission',
-        version='0.0.1',
-        py_modules=['transmission'],
-        description=('Tools for inferring symbiont transmission mode from'
-                     'metagenomic data'),
-        url='http://github.com/mpjuers/transmission',
-        author='Mark Juers',
-        author_email='mpjuers@indiana.edu',
-        license='GPL>=3',
-        packages=find_packages(),
-        include_pakage_data=True,
-        install_requires=[
-            'click',
-            'msprime',
-            'numpy',
-            'rpy2',
-            'tqdm'
-            ],
-        entry_points='''
+    name='transmission',
+    version='0.0.1',
+    py_modules=['transmission'],
+    description=('Tools for inferring symbiont transmission mode from'
+                 'metagenomic data'),
+    url='http://github.com/mpjuers/transmission',
+    author='Mark Juers',
+    author_email='mpjuers@indiana.edu',
+    license='GPL>=3',
+    packages=find_packages(),
+    include_pakage_data=True,
+    install_requires=[
+        'click',
+        'msprime',
+        'numpy',
+        'rpy2',
+        'tqdm'
+        ],
+    entry_points='''
         [console_scripts]
         transmission-priorgen=transmission.transmission:simulate_prior_stats
-        ''',
-        zip_safe=False
+    ''',
+    zip_safe=False
     )
