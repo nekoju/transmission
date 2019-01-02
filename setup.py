c246c61ac00642d725eae8f389b978c454d23702
mpjuers <mpjuers@indiana.edu>
2019-01-02 12:55:56 -0500

    changed to v0.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import pypandoc
 pypandoc.convert_file('README.md', 'rst', outputfile='README.rst')
 setup(
     name='transmission',
-    version='0.0.1',
+    version='0.0.2',
     py_modules=['transmission'],
     description=('Tools for inferring symbiont transmission mode from'
                  'metagenomic data'),
